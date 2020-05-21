73a5eae8a7d2b62c299735e5d30e4ec3b14818cf
JoshH100 <2730968+Princemachiavelli@users.noreply.github.com>
2020-05-20 22:28:38 -0600

    manually applied dmenumatchtop 'patch' so that dmenu appears in same location as bar

--- config.def.h
+++ config.def.h
@@ -65,7 +65,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbordercolor, "-sf", selfgcolor, "-b", NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbordercolor, "-sf", selfgcolor, topbar ? NULL : "-b" , NULL };
 static const char *termcmd[]  = { "st", NULL };
 
 static Key keys[] = {
